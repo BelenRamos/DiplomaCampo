137ab601c886a6ced01c83997c3cd6de762b2511
Belen Ramos <m.b.ramos0204@gmail.com>
2024-07-08 19:11:37 -0300

    Reportes postulantes

--- TP Soft-Diploma/ProyectoRRHH/Presentacion/Formulario de Reporte/FormReportePostulantes.Designer.cs
+++ TP Soft-Diploma/ProyectoRRHH/Presentacion/Formulario de Reporte/FormReportePostulantes.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerarReportePostulantes = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvDataPostulantes = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataPostulantes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerarReportePostulantes
+            // 
+            this.btnGenerarReportePostulantes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerarReportePostulantes.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold);
+            this.btnGenerarReportePostulantes.ForeColor = System.Drawing.Color.RosyBrown;
+            this.btnGenerarReportePostulantes.Location = new System.Drawing.Point(318, 328);
+            this.btnGenerarReportePostulantes.Name = "btnGenerarReportePostulantes";
+            this.btnGenerarReportePostulantes.Size = new System.Drawing.Size(105, 35);
+            this.btnGenerarReportePostulantes.TabIndex = 11;
+            this.btnGenerarReportePostulantes.Text = "Guardar PDF";
+            this.btnGenerarReportePostulantes.UseVisualStyleBackColor = true;
+            this.btnGenerarReportePostulantes.Click += new System.EventHandler(this.btnGenerarReportePostulantes_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(19, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 86);
+            this.panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(71)))), ((int)(((byte)(97)))));
+            this.label5.Location = new System.Drawing.Point(272, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Reporte de Postulantes";
+            // 
+            // dgvDataPostulantes
+            // 
+            this.dgvDataPostulantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataPostulantes.Location = new System.Drawing.Point(19, 100);
+            this.dgvDataPostulantes.Name = "dgvDataPostulantes";
+            this.dgvDataPostulantes.Size = new System.Drawing.Size(711, 212);
+            this.dgvDataPostulantes.TabIndex = 9;
+            // 
+            // FormReportePostulantes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(62)))));
+            this.ClientSize = new System.Drawing.Size(748, 369);
+            this.Controls.Add(this.btnGenerarReportePostulantes);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvDataPostulantes);
+            this.Name = "FormReportePostulantes";
             this.Text = "FormReportePostulantes";
+            this.Load += new System.EventHandler(this.FormReportePostulantes_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataPostulantes)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerarReportePostulantes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvDataPostulantes;
     }
 }
--- TP Soft-Diploma/ProyectoRRHH/Presentacion/Formulario de Reporte/FormReportePostulantes.cs
+++ TP Soft-Diploma/ProyectoRRHH/Presentacion/Formulario de Reporte/FormReportePostulantes.cs
@@ -1,20 +1,56 @@
-﻿using System;
+﻿using Modelo;
+using Negocio;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace Presentacion.Formulario_de_Reporte
 {
     public partial class FormReportePostulantes : Form
     {
+        private NegPostulantes negPostulantes;
+        private NegReportes negReportes;
+
         public FormReportePostulantes()
         {
             InitializeComponent();
+            negPostulantes = NegPostulantes.ObtenerInstancia();
+            negReportes = NegReportes.ObtenerInstancia();
+        }
+
+        private void FormReportePostulantes_Load(object sender, EventArgs e)
+        {
+            List<Postulantes> postulantes = negPostulantes.ObtenerTodosLosPostulantes();
+            dgvDataPostulantes.DataSource = postulantes;
+        }
+
+        private void btnGenerarReportePostulantes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Postulantes> postulantes = (List<Postulantes>)dgvDataPostulantes.DataSource;
+                if (postulantes == null || postulantes.Count == 0)
+                {
+                    MessageBox.Show("No hay datos de postulantes para generar el reporte.");
+                    return;
+                }
+
+                string rutaDirectorio = @"C:\Reportes";
+                if (!System.IO.Directory.Exists(rutaDirectorio))
+                {
+                    System.IO.Directory.CreateDirectory(rutaDirectorio);
+                }
+
+                string rutaArchivo = System.IO.Path.Combine(rutaDirectorio, "reporte_postulantes.pdf");
+                negReportes.GenerarReportePostulantes(postulantes, rutaArchivo);
+                MessageBox.Show("Reporte de Postulantes generado exitosamente en " + rutaArchivo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al generar el reporte: {ex.Message}");
+            }
         }
     }
 }
+
